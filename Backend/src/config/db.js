6d2a59cdc6331d271113006f326d9699778ba75a
Dev-DsC <daltonsteve22@gmail.com>
2025-07-17 19:25:53 -0500

    prueba de gitignore 3

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,15 +1,23 @@
-const { Pool } = require('pg');
+const pg = require('pg');
+require('dotenv').config();
+
+const{Pool}= pg;
+
 
 const db = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
-db.connect()
-    .then(() => console.log('database connected'))
-    .catch(err => console.error('Database connection error:', err));
+
+try {
+    db.query('SELECT NOW()');
+    console.log('database connected');
+} catch (error) {
+    console.log('error connection');
+}
 
 module.exports = db;
-//algo
